f364f18039941158800c6ff54474bbf67e922eb0
Adityanaik4545 <adityank23@outlook.com>
2025-06-21 20:07:54 +0530

    project setup, folder structure

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const axios = require('axios')
+const app = express()
+const port = 3000
+app.use("view engine","ejs")
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
