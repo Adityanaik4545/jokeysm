696060d81a2d8cbb8d2420043c9693b0b387463c
Adityanaik4545 <adityank23@outlook.com>
2025-06-22 01:54:37 +0530

    achived basic functionality requesting jokes and displaying it

--- app.js
+++ app.js
@@ -2,10 +2,40 @@ const express = require('express')
 const axios = require('axios')
 const app = express()
 const port = 3000
+let selectedCategory;
 app.set("view engine","ejs")
 app.use(express.static("public"))
 app.get('/', (req, res) => {
-  res.render('index')
+  res.render('index',{setup:"What kind of joke you wanna here?, let me know 😁"})
+})
+app.get('/any', (req, res) => {
+  selectedCategory="Any"
+})
+app.get('/programming', async(req, res) => {
+  selectedCategory="Programming"
+})
+app.get('/dark', async(req, res) => {
+  selectedCategory="Dark"
+})
+app.get('/spooky', async(req, res) => {
+  selectedCategory="Spooky"
+})
+app.get('/misc', async(req, res) => {
+  selectedCategory="Misc"
+})
+app.get('/pun', async(req, res) => {
+  selectedCategory="Pun"
+})
+app.get('/christmas', async(req, res) => {
+  selectedCategory="Christmas"
+})
+app.get('/generate', async(req, res) => {
+  try {
+    const response= await axios.get(`https://v2.jokeapi.dev/joke/${selectedCategory}`)
+    res.render('index',{setup:JSON.stringify(response.data.setup),delivery:JSON.stringify(response.data.delivery)})
+  } catch (error) {
+    
+  }
 })
 
 app.listen(port, () => {
