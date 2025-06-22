d4af66639662e7f60428b8758d4246b0ad924d4b
Adityanaik4545 <adityank23@outlook.com>
2025-06-23 01:19:14 +0530

    made responsive

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require('express')
 const axios = require('axios')
 const app = express()
 const port = 3000
-let selectedCategory;
 app.set("view engine","ejs")
 app.use(express.static("public"))
 app.get('/', (req, res) => {
